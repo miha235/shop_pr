f4b7406ee8b89f92d04fc0e97348dd184f97c6a2
Victoria <Victoria23miha@gmail.com>
2024-11-11 23:10:43 +0300

    iteration

--- catalog/models.py
+++ catalog/models.py
@@ -1,15 +1,15 @@
-'''Задание 2
-В приложении каталога создайте модели:
-
-Product,
-Category.
-Опишите для них начальные настройки.'''
-
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name="Название категории")
-    description = models.TextField(blank=True, verbose_name="Описание")
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name="Название категории"
+    )
+    description = models.TextField(
+        blank=True,
+        verbose_name="Описание"
+    )
 
     class Meta:
         verbose_name = "Категория"
@@ -21,13 +21,43 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Название продукта")
-    description = models.TextField(blank=True, verbose_name="Описание")
-    image = models.ImageField(upload_to='catalog/', verbose_name="Изображение")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='catalog', verbose_name="Категория")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название продукта"
+    )
+    description = models.TextField(
+        blank=True,
+        verbose_name="Описание"
+    )
+    image = models.ImageField(
+        upload_to='catalog/',
+        verbose_name="Изображение"
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Цена"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='catalog',
+        verbose_name="Категория"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Дата последнего изменения"
+    )
+
+    view_counter = models.PositiveIntegerField(
+        verbose_name = "Счетчик просмотров",
+        help_text = "Укажите количество просмотров",
+        default = 0
+    )
 
     class Meta:
         verbose_name = "Продукт"
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy , reverse
 from django.views.generic import ListView , DetailView , CreateView , UpdateView , DeleteView
 
 from .models import Product
@@ -13,20 +13,12 @@ class ProductlistView(ListView):
 class ProductdetailView(DetailView):
     model = Product
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.view_counter += 1
+        self.object.save()
+        return self.object
 
-# Создайте новую модель блоговой записи со следующими полями:
-
-# заголовок;
-# slug (реализовать через CharField);
-# содержимое;
-# превью (изображение);
-# дата создания;
-# признак публикации;
-# количество просмотров.
-
-# Для работы с блогом реализуйте CRUD для новой модели.
-# CRUD реализуйте на основе CBV (ListView, DetailView, CreateView, UpdateView, DeleteView)
-# Соблюдайте нейминг шаблонов для CBV контроллеров - …_list.html, …_detail.html, …_form.html.
 
 class ProductcreateView(CreateView):
     model = Product
@@ -37,6 +29,9 @@ class ProductupdateView(UpdateView):
     model = Product
     fields = ("name" , "description" , "image" , "price" , "category")
     success_url = reverse_lazy ( 'catalog:product_list' )
+
+    def get_success_url(self):
+        return reverse('catalog:product_detail',args = [self.kwargs.get('pk')])
 
 class ProductdeleteView(DeleteView):
     model = Product
